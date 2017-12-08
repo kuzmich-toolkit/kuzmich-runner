763790db2c34c665308bdf890c3c1ad671e79cd0
Dmitriy Merkurev <dmerkurev@avito.ru>
2017-12-08 03:14:04 +0300

    small refactoring generated code by cobra

--- cmd/root.go
+++ cmd/root.go
@@ -9,23 +9,16 @@ import (
 	"github.com/spf13/viper"
 )
 
-var cfgFile string
+var configurationFile string
 
-var rootCmd = &cobra.Command{
+var rootCommand = &cobra.Command{
 	Use:   "kuzmich-runner",
 	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long:  `TODO: info about kuzmich-runner`,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	if err := rootCmd.Execute(); err != nil {
+	if err := rootCommand.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
@@ -33,34 +26,30 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.kuzmich-runner.yaml)")
+	rootCommand.PersistentFlags().StringVar(
+		&configurationFile,
+		"config",
+		"",
+		"config file (default is $HOME/.kuzmich-runner.yaml)",
+	)
 }
 
-// initConfig reads in config file and ENV variables if set.
 func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
+	if configurationFile != "" {
+		viper.SetConfigFile(configurationFile)
 	} else {
-		// Find home directory.
 		home, err := homedir.Dir()
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
 
-		// Search config in home directory with name ".kuzmich-runner" (without extension).
 		viper.AddConfigPath(home)
 		viper.SetConfigName(".kuzmich-runner")
 	}
 
-	viper.AutomaticEnv() // read in environment variables that match
+	viper.AutomaticEnv()
 
-	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Println("Using config file:", viper.ConfigFileUsed())
 	}
--- cmd/run.go
+++ cmd/run.go
@@ -5,20 +5,16 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var runCmd = &cobra.Command{
+var runCommand = &cobra.Command{
 	Use:   "run",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Run instrumentation tests",
+	Long:  `TODO: add full description`,
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("run called")
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(runCmd)
+	rootCommand.AddCommand(runCommand)
 }
